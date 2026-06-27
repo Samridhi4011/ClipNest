7db9e5989564660bed632148d9cd87021db3d297
Samridhi4011 <samridhi3219@gmail.com>
2026-06-27 18:56:37 +0530

    initialize ClipNest landing page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>ClipNest</h1>
+      <p>Your smart clipboard manager.</p>
+    </div>
+  );
+}
+
+export default App;
